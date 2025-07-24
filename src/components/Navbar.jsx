65169a21c29ec200005df199d7346b7bba317e12
alooshydabajeh001-arch <alooshy.dabajeh001@gmail.com>
2025-07-24 11:31:55 -0400

    Add Navbar component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,15 +1,1 @@
-import { Box, Flex, Heading } from '@chakra-ui/react';
-
-function Navbar() {
-  return (
-    <Box bg="teal.500" color="white" p={4}>
-      <Flex justify="center">
-        <Heading size="md">Auralis</Heading>
-      </Flex>
-    </Box>
-  );
-}
-
-export default Navbar;
-
-
+$json["message"]["fileStructure"]["src"]["components"]["Navbar.jsx"]|
